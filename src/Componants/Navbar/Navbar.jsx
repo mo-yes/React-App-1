011e96ce8a4a975b2756fb8aaf5c0eef72100b1a
mo-yes <mohamed490216@gmail.com>
2025-07-24 06:36:11 +0300

    Styled active link with color and border, removed Home link

--- src/Componants/Navbar/Navbar.jsx
+++ src/Componants/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import "./Navbar.css"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 export default function Navbar() {
 
@@ -25,17 +25,17 @@ export default function Navbar() {
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
+        {/* <li className="nav-item">
+          <NavLink className="nav-link my-link   fs-5 fw-bold" aria-current="page" to="/">Home</NavLink>
+        </li> */}
         <li className="nav-item">
-          <Link className="nav-link my-link   fs-5 fw-bold" aria-current="page" to="/">Home</Link>
+          <NavLink className="nav-link my-link fs-5 fw-bold" aria-current="page" to="/about">About</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link my-link fs-5 fw-bold" aria-current="page" to="/about">About</Link>
+          <NavLink className="nav-link  my-link fs-5 fw-bold" to="/portfolio">Portfolio</NavLink>
         </li>
         <li className="nav-item">
-          <Link className="nav-link  my-link fs-5 fw-bold" to="/portfolio">Portfolio</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link  my-link fs-5 fw-bold" to="/contact">Contact</Link>
+          <NavLink className="nav-link  my-link fs-5 fw-bold" to="/contact">Contact</NavLink>
         </li>
       </ul>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.js"
 import "@fortawesome/fontawesome-free/css/all.min.css"
 
+
 createRoot(document.getElementById('root')).render(
     <App />
 )
